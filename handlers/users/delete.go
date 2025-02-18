135d606ac0c8e15360f24e4fadae5800b06c806b
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-02-18 15:13:39 -0800

    Add cleanup steps to remove all images from S3 when user deletes their account

--- handlers/users/delete.go
+++ handlers/users/delete.go
@@ -1,12 +1,15 @@
 package userHandlers
 
 import (
+	"JourneyAppServer/aws"
 	"JourneyAppServer/db"
 	"JourneyAppServer/types"
 	"context"
 	"encoding/json"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 	"net/http"
 	"time"
 )
@@ -38,8 +41,56 @@ func deleteAccount(username string) (types.DeleteAccountResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
+	userCollection := db.MongoClient.Database(db.DbName).Collection(db.UserCollection)
 	entryCollection := db.MongoClient.Database(db.DbName).Collection(db.EntriesCollection)
-	_, err := entryCollection.DeleteMany(ctx, bson.M{"username": username})
+
+	pipeline := mongo.Pipeline{}
+
+	matchUser := bson.D{{Key: "$match", Value: bson.D{
+		{Key: "username", Value: username},
+	}}}
+	pipeline = append(pipeline, matchUser)
+
+	cursor, err := entryCollection.Aggregate(ctx, pipeline)
+	if err != nil {
+		log.Println("Aggregate all entries error: ", err)
+		return types.DeleteAccountResponse{
+			Success: false,
+		}, err
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var EntryImages struct {
+			IDM    string   `bson:"_id"`
+			ID     string   `bson:"id"`
+			Images []string `bson:"images"`
+		}
+		if err := cursor.Decode(&EntryImages); err != nil {
+			log.Println("Error decoding individual entry:", err)
+			return types.DeleteAccountResponse{
+				Success: false,
+			}, err
+		}
+
+		if len(EntryImages.Images) > 0 {
+			for _, imageKey := range EntryImages.Images {
+				if res := aws.DeleteImage(imageKey); !res.Success {
+					log.Println("Error deleting individual image: ", imageKey)
+				} else {
+					log.Println("Successfully deleted image: ", imageKey, "!")
+				}
+			}
+		}
+	}
+	if err := cursor.Err(); err != nil {
+		log.Println("Cursor encountered an error: ", err)
+		return types.DeleteAccountResponse{
+			Success: false,
+		}, err
+	}
+
+	_, err = entryCollection.DeleteMany(ctx, bson.M{"username": username})
 	if err != nil {
 		fmt.Println("Error deleting all entries from the database: ", err)
 		return types.DeleteAccountResponse{
@@ -48,7 +99,6 @@ func deleteAccount(username string) (types.DeleteAccountResponse, error) {
 	}
 
 	// then delete the account
-	userCollection := db.MongoClient.Database(db.DbName).Collection(db.UserCollection)
 	_, err = userCollection.DeleteOne(ctx, bson.M{"username": username})
 	if err != nil {
 		fmt.Println("Error deleting the user from the database: ", err)
